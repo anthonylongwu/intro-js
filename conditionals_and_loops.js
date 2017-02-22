68cc5ce79dea07411894e08a14045c5d47dcbed5
Anthony Wu <anthonylongwu19@gmail.com>
2017-02-22 00:04:43 -0800

    initial commit

---
+++ conditionals_and_loops.js
@@ -0,0 +1,99 @@
+// ruby conditional
+// x
+// if x > 50
+// puts "high number"
+// elsif x == 30
+// puts "my fave number"
+// else 
+// puts "low number"
+// end
+
+// js conditional
+// var x = 20;
+// if (x > 50); {
+//    console.log("high numner");
+// } else if (x == 30) {
+//    console.log("my favorite number");
+// } else {
+//   console.log("low number");
+//   }
+
+// ruby equality 
+// 5 == 5 #true
+// 5 == "5" #false
+// 5 != "5" #true
+
+// JS equality
+// console.log(5 == 5); // True
+// console.log(5 == "5"); // true
+// console.log(5 === "5"); // false
+// console.log([5] == 5); // true
+// console.log(5 !== "5"); // true 
+// console.log(5 != "5"); // false
+
+
+// ruby falseys
+// nil 
+// false 
+
+// js falseys
+// if (!false) {console.log("wat")};
+// if (!undefined) {console.log("wat")};
+// if (!null) {console.log("wat")};
+// if (!NaN) {console.log("wat")};
+// if (!0) {console.log("wat")};
+// if (!"") {console.log("wat")};
+
+// ruby rand nums
+// rand(1..100)
+
+// js rand nums
+// console.log(Math.floor(Math.random() * 100));
+
+// ruby loops 
+// i = 0
+// text = ""
+// while i < 10
+//  text = text + "The Number is " + i 
+//  i += 1
+// end
+// puts text
+
+
+// js while loop
+// var i = 0;
+// var text = "";
+// while (i < 10) {
+//   text = text + "The number is " + i;
+//   i++;
+//   }
+//       console.log(text);
+
+
+// ruby for loop
+// for i in 0..10
+// puts i
+// end
+
+// js for loop
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+//   }
+
+// ruby each through array
+// fruits = ["banana", "apple", "orange", "mango", "kiwi"]
+// fruits.each do |fruit|
+// puts fruit
+// end
+
+// js each through array
+// var fruits = ["banana", "apple", "orange", "mango", "kiwi"];
+// for (var i = 0; i < fruits.length; i++){
+//   console.log(fruits[i]);
+//   }
+
+
+
+
+
+  
